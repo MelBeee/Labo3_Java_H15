4cac596ad738cd3164a69e743590affb234a1220
Melissa <mel.beee@deepteal.com>
2015-02-11 11:48:41 -0500

    Plein d'updates

--- Jedit.java
+++ Jedit.java
@@ -1,5 +1,41 @@
 // Jedit.java
-// 
+// Jedit est un éditeur de texte très minimaliste en mode ligne
 // Fait par Melissa Boucher et Francis Thibodeau 
 // 13 Fevrier 2015
 
+public class Jedit
+{
+   public void editer( String args[] )
+   {
+      if(args.length > 0 && args.length <= 2)
+		{
+         String option = "" ;
+         String nomFichier = "";
+         
+         if(args.length == 1)
+         {
+            nomFichier = args[0];
+         }
+         else
+         {
+            option = args[0];
+            nomFichier = args[1];
+         }
+         
+         System.out.println(option);
+         System.out.println(nomFichier); 
+		}
+		else
+		{
+         // Si l'utilisateur a entré zero ou plus de deux parametre
+			System.out.println( "Entrez minimalement un parametre et maximalement 2 parametres.");
+		}
+   }
+   
+   public static void main( String args[] )
+   {
+      Jedit app = new Jedit();
+      app.editer( args );
+   }
+   
+}
