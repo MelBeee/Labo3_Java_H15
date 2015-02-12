e26a5ade0386e98fd823bfc9b8890fd95c2b163f
MelBeee <mel.beee@deepteal.com>
2015-02-11 20:37:57 -0500

    Toute fonctionne =D :end:

--- Jedit.java
+++ Jedit.java
@@ -2,15 +2,15 @@
 // Jedit est un éditeur de texte très minimaliste en mode ligne
 // Fait par Melissa Boucher et Francis Thibodeau 
 // 13 Fevrier 2015
-
+import java.io.*;
 public class Jedit
 {
-	private final int MIN_PARAM = 0;
-	private final int MAX_PARAM = 2; 
-	private final char OUI = 'O';
-	private final char NON = 'N';
+	private static final int MIN_PARAM = 0;
+	private static final int MAX_PARAM = 2; 
+	private static final char OUI = 'O';
+	private static final char NON = 'N';
 	
-	public boolean verifiernbreparametre(String args[])
+	public static boolean verifiernbreparametre(String args[])
 	{
 		boolean paramValide = true; 
 
@@ -19,7 +19,7 @@ public class Jedit
 			paramValide = false;
 			System.out.println("Il doit y avoir minimum 1 parametre et maximum 2 parametres, fichier non cree.");
 		}
-		else if (args[MIN_PARAM] != "-n" && args.length == MAX_PARAM)
+		else if (!args[MIN_PARAM].equals("-n") && args.length == MAX_PARAM)
 		{
 			System.out.println("Vous avez entre une option invalide, fichier non cree.");
 			paramValide = false;	
@@ -27,9 +27,8 @@ public class Jedit
 		return paramValide;
 	}
 	
-	public boolean fichierexiste(String nomFichier)
+	public void fichierexiste(String nomFichier, String args[])
 	{
-		boolean continuer = true; 
 		char resultat;
 		
 		System.out.println("Un fichier portant le nom " + nomFichier + " existe deja.");
@@ -37,37 +36,28 @@ public class Jedit
 		
 		try
 		{
-			do
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			resultat = (char)reader.read();
+			
+			if(Character.toUpperCase(resultat) == OUI)
 			{
-				BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-				resultat = reader.read();
-				
-				if(Character.toUpperCase(resultat).equals(OUI))
-				{
-					System.out.println("Le fichier sera modifie.")
-				}
-				else if(Character.toUpperCase(resultat).equals(NON))
-				{
-					System.out.println("Le fichier ne sera pas modifie.")
-					continuer = false; 
-				}
-				else
-				{
-					System.out.println("Choix invalide, recommencez.")
-				}
-				
-			}while(Character.toUpperCase(resultat).equals(OUI) || 
-				   Character.toUpperCase(resultat).equals(NON));
-			
-			
+				System.out.println("Le fichier sera modifie.");
+				ecriredansfichier(args, nomFichier);
+			}
+			else if(Character.toUpperCase(resultat) == NON)
+			{
+				System.out.println("Le fichier ne sera pas modifie.");
+			}
+			else
+			{
+				System.out.println("Choix invalide.");
+			}
 		}
 		catch(IOException e)
 		{
 			System.err.println(e);
 			System.exit(1);
 		}
-		
-		return continuer; 
 	}
 
 	public void ecriredansfichier(String args[], String nomFichier)
@@ -78,7 +68,23 @@ public class Jedit
 		
 		try
 		{
-		
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			PrintWriter writer = new PrintWriter( new BufferedWriter( new FileWriter(nomFichier)));
+			
+			int numLigne = 1;
+			String ligne = reader.readLine();
+			
+			
+			while(!ligne.trim().isEmpty())
+			{
+				if(ecrireNumero)
+					writer.print(numLigne + "- ");
+				writer.print(ligne + "\n");
+				
+				numLigne ++; 
+				ligne = reader.readLine();
+			}
+			writer.close();
 		}
 		catch(IOException e)
 		{
@@ -93,15 +99,14 @@ public class Jedit
 	{
 		String nomDuFichier = args[0];
         File fichierUtilise;
-		boolean continuer = true;
 
         if (args.length > 1) // si l'utilisateur a entré une option, le nom du fichier devrait etre en 2e position
-			nomFichier = args[1]; 
+			nomDuFichier = args[1]; 
 			
 		fichierUtilise = new File(nomDuFichier);
 		if(fichierUtilise.exists())
-			continuer = fichierexiste(nomDuFichier));
-		else if(!fichierUtilise.exists() || continuer)
+			fichierexiste(nomDuFichier, args);
+		else
 			ecriredansfichier(args, nomDuFichier);
 	}
 
@@ -109,7 +114,7 @@ public class Jedit
 	{
 		Jedit app = new Jedit();
 		
-		if(verifiernbreparametre(args[])
+		if(verifiernbreparametre(args))
 			app.editer( args );
 	}
 }
