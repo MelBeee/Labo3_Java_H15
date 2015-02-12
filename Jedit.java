ad0513e34e640eabb208014242bc6c2e254a3a36
Francis Thibodeau <francis.thbd@gmail.com>
2015-02-12 11:18:53 -0500

    Commentaire

--- Jedit.java
+++ Jedit.java
@@ -6,11 +6,19 @@ import java.io.*;
 import javax.swing.JOptionPane;
 public class Jedit
 {
+	// Déclaration des variables
 	private static final int MIN_PARAM = 0;
 	private static final int MAX_PARAM = 2; 
 	private static final char OUI = 'O';
 	private static final char NON = 'N';
 	
+	// Vérification du nombre de parametre entrer par l'utilisateur.
+	// Il doit y avoir 1 ou 2 parametre.
+	// L'utilisateur peut entrer une seule option : -n qui numerote les lignes.
+	// Si la commande de l'utilisateur est valide, et que l'option est innexistante ou valide, le fichier est créer.
+	// Si une valeure est invalide, le fichier n'es pas créé.
+	
+	// La fonction retourne un bool True si tout est valide.
 	public static boolean verifiernbreparametre(String args[])
 	{
 		boolean paramValide = true; 
@@ -32,6 +40,11 @@ public class Jedit
 		return paramValide;
 	}
 	
+	// Cette fonction informe qu'un fichier du meme nom existe et traite la requete de l'utilisateur.
+	// La fonction prend la reponse de l'utilisateur, la met en majuscule et traite l'entrée.
+	// Si l'entrée est O, le fichier existant sera modifié.
+	// Si l'entrée est N, le fichier existant ne sera pas modifié.
+	// Si l'entrée est autre chose que O/N, l'entrée est invalide.
 	public void fichierexiste(String nomFichier, String args[])
 	{
 		char resultat;
@@ -65,6 +78,9 @@ public class Jedit
 		}
 	}
 
+	// Cette fonction permet a l'utilisateur d'insérer ce qu'il a écrit en console dans une fichier text.
+	// Si les donnée founie par l'utilisateur sont valide et qu'il demande la numération des lignes(-n) les lignes sont numéroté.
+	// Cette fonction retire aussi les espaces en trop au début et a la fin du string.
 	public void ecriredansfichier(String args[], String nomFichier)
 	{
 		boolean ecrireNumero = true; 
@@ -98,6 +114,9 @@ public class Jedit
 		}
 	}
 
+	// Cette fonction vérifie si un fichier du meme nom que celui fournit par l'utilisateur existe.
+	// Si le fichier existe la fonction appelle fichierexiste
+	// Sinon, la fonction appelle ecriredansfichier
 	public void editer(String args[])
 	{
 		String nomDuFichier = args[0];
@@ -113,6 +132,7 @@ public class Jedit
 			ecriredansfichier(args, nomDuFichier);
 	}
 
+	// Main
 	public static void main( String args[] )
 	{
 		Jedit app = new Jedit();
